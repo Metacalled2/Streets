08966027c881fd49ae2006b9a98f1bf62cba819a
Metacalled <73370902+Metacalled2@users.noreply.github.com>
2021-01-15 23:06:19 +0000

    Update ESP_LIST.lua

--- Tables/ESP_LIST.lua
+++ Tables/ESP_LIST.lua
@@ -1,1 +1,44 @@
-[{"Font":"Creepster","userid":1898371630,"TagIdentification":" [ Scripter ] ","ColorTable":[255,0,0],"TagText":"Metacalled"},{"Font":"Creepster","userid":463809410,"TagIdentification":" [ Anna ] ","ColorTable":[255,20,147],"TagText":"Velurity"},{"Font":"Creepster","userid":668345977,"TagIdentification":" [ Eren ] ","ColorTable":[0,0,0],"TagText":"Aucestre"},{"Font":"Creepster","userid":139759589,"TagIdentification":" [ Eren ] ","ColorTable":[0,0,0],"TagText":"Aucestre"},{"Font":"Creepster","userid":2261302324,"TagIdentification":" [ Eren ] ","ColorTable":[0,0,0],"TagText":"Aucestre"},{"Font":"Creepster","userid":1762590381,"TagIdentification":" [ Kate ] ","ColorTable":[255,20,147],"TagText":"Atuskiie"}]
+      "Font":"Fredoka",
+      "userid":454243468,
+      "TagIdentification":" [ Retard Girl Group ] ",
+      "ColorTable":[
+         74,
+         66,
+         135
+      ],
+      "TagText":"Shyanne"
+   },
+   {
+      "Font":"Fredoka",
+      "userid":192617749,
+      "TagIdentification":" [ Retard Girl Group ] ",
+      "ColorTable":[
+         74,
+         66,
+         135
+      ],
+      "TagText":"Canan"
+   },
+   {
+      "Font":"Fredoka",
+      "userid":846150720,
+      "TagIdentification":" [ Retard Girl Group ] ",
+      "ColorTable":[
+         74,
+         66,
+         135
+      ],
+      "TagText":"Jayda"
+   },
+   {
+      "Font":"Fredoka",
+      "userid":899010159,
+      "TagIdentification":" [ Retard Girl Group ] ",
+      "ColorTable":[
+         74,
+         66,
+         135
+      ],
+      "TagText":"Jamie / Shyanne"
+   }
+]
