6b0064d0d5894a25e9faea56f6b621bbbfd3bdb0
Metacalled <73370902+Metacalled2@users.noreply.github.com>
2021-01-25 21:10:58 +0000

    Update ESP_LIST.lua

--- Tables/ESP_LIST.lua
+++ Tables/ESP_LIST.lua
@@ -2,347 +2,859 @@
    {
       "Font":"Creepster",
       "userid":1898371630,
-      "Color":"Red",
-      "TagIdentification":"SC",
+      "TagIdentification":" [ SCRIPTER ] ",
+      "ColorTable":[
+         255,
+         0,
+         0
+      ],
       "TagText":"Metacalled"
    },
    {
       "Font":"Creepster",
       "userid":713824573,
-      "Color":"Red",
-      "TagIdentification":"SC",
+      "TagIdentification":" [ SCRIPTER ] ",
+      "ColorTable":[
+         255,
+         0,
+         0
+      ],
       "TagText":"Metacalled"
    },
    {
       "Font":"Creepster",
       "userid":463809410,
-      "Color":"Pink",
-      "TagIdentification":"Custom/VELURITY",
+      "TagIdentification":" [ VELURITY ] ",
+      "ColorTable":[
+         255,
+         20,
+         147
+      ],
       "TagText":"Anna"
    },
    {
       "Font":"Creepster",
       "userid":668345977,
-      "Color":"Purple",
-      "TagIdentification":"Custom/AUCESTRE",
+      "TagIdentification":" [ AUCESTRE ] ",
+      "ColorTable":[
+         0,
+         0,
+         0
+      ],
       "TagText":"Eren"
    },
    {
       "Font":"Creepster",
       "userid":139759589,
-      "Color":"Purple",
-      "TagIdentification":"Custom/AUCESTRE",
+      "TagIdentification":" [ AUCESTRE ] ",
+      "ColorTable":[
+         0,
+         0,
+         0
+      ],
       "TagText":"Eren"
    },
    {
       "Font":"Creepster",
       "userid":2261302324,
-      "Color":"Purple",
-      "TagIdentification":"Custom/AUCESTRE",
+      "TagIdentification":" [ AUCESTRE ] ",
+      "ColorTable":[
+         0,
+         0,
+         0
+      ],
       "TagText":"Eren"
    },
    {
       "Font":"Creepster",
       "userid":1762590381,
-      "Color":"RedPink",
-      "TagIdentification":"Custom/BITCH",
+      "TagIdentification":" [ BITCH ] ",
+      "ColorTable":[
+         255,
+         20,
+         147
+      ],
       "TagText":"Kate"
    },
    {
       "Font":"Fredoka",
       "userid":147182208,
-      "Color":"Grl",
-      "TagIdentification":"Custom/RETARD GIRL GROUP",
+      "TagIdentification":" [ RETARD GIRL GROUP ] ",
+      "ColorTable":[
+         74,
+         66,
+         135
+      ],
       "TagText":"Spudaddles"
    },
    {
       "Font":"Fredoka",
       "userid":1083587286,
-      "Color":"Grl",
-      "TagIdentification":"Custom/RETARD GIRL GROUP",
+      "TagIdentification":" [ RETARD GIRL GROUP ] ",
+      "ColorTable":[
+         74,
+         66,
+         135
+      ],
       "TagText":"Jamie"
    },
    {
       "Font":"Fredoka",
       "userid":454243468,
-      "Color":"Grl",
-      "TagIdentification":"Custom/RETARD GIRL GROUP",
+      "TagIdentification":" [ RETARD GIRL GROUP ] ",
+      "ColorTable":[
+         74,
+         66,
+         135
+      ],
       "TagText":"Shyanne"
    },
    {
       "Font":"Fredoka",
       "userid":192617749,
-      "Color":"Grl",
-      "TagIdentification":"Custom/RETARD GIRL GROUP",
+      "TagIdentification":" [ RETARD GIRL GROUP ] ",
+      "ColorTable":[
+         74,
+         66,
+         135
+      ],
       "TagText":"Canan"
    },
    {
       "Font":"Fredoka",
       "userid":846150720,
-      "Color":"Grl",
-      "TagIdentification":"Custom/RETARD GIRL GROUP",
+      "TagIdentification":" [ RETARD GIRL GROUP ] ",
+      "ColorTable":[
+         74,
+         66,
+         135
+      ],
       "TagText":"Jayda"
    },
    {
       "Font":"Fredoka",
       "userid":899010159,
-      "Color":"Grl",
-      "TagIdentification":"Custom/RETARD GIRL GROUP",
+      "TagIdentification":" [ Retard Girl Group ] ",
+      "ColorTable":[
+         74,
+         66,
+         135
+      ],
       "TagText":"Jamie / Shyanne"
    },
    {
       "Font":"Creepster",
       "userid":1762400000,
-      "Color":"Grl",
-      "TagIdentification":"Custom/PASSAROUND",
+      "TagIdentification":" [ PASSAROUND ] ",
+      "ColorTable":[
+         74,
+         66,
+         135
+      ],
       "TagText":"KD"
    },
    {
       "Font":"Creepster",
       "userid":92616350,
-      "Color":"Grl",
-      "TagIdentification":"Custom/PASSAROUND",
+      "TagIdentification":" [ PASSAROUND ] ",
+      "ColorTable":[
+         74,
+         66,
+         135
+      ],
       "TagText":"Libby"
    },
    {
       "Font":"Creepster",
       "userid":781004960,
-      "Color":"Grl",
-      "TagIdentification":"Custom/PASSAROUND",
+      "TagIdentification":" [ PASSAROUND ] ",
+      "ColorTable":[
+         74,
+         66,
+         135
+      ],
       "TagText":"Zoe"
    },
    {
       "Font":"Creepster",
       "userid":2258301745,
-      "Color":"Grl",
-      "TagIdentification":"Custom/PASSAROUND",
+      "TagIdentification":" [ PASSAROUND ] ",
+      "ColorTable":[
+         74,
+         66,
+         135
+      ],
       "TagText":"Kim"
    },
    {
       "Font":"Creepster",
       "userid":1410480510,
-      "Color":"Green",
-      "TagIdentification":"Custom/HELPER",
-      "TagText":"Alex"
+      "TagIdentification":" [ HELPER ] ",
+      "ColorTable":[
+         0,
+         255,
+         0
+      ],
+      "TagText":"Tester"
    },
    {
       "Font":"Creepster",
       "userid":1440862719,
-      "Color":"Green",
-      "TagIdentification":"Custom/HELPER",
-      "TagText":"Alexr"
+      "TagIdentification":" [ HELPER ] ",
+      "ColorTable":[
+         0,
+         255,
+         0
+      ],
+      "TagText":"Tester"
    },
    {
       "Font":"Creepster",
       "userid":105183043,
-      "Color":[
+      "TagIdentification":" [ SCRIPTER ] ",
+      "ColorTable":[
          20,
          247,
          244
       ],
-      "TagIdentification":"SC",
       "TagText":"Charlie"
    },
    {
       "Font":"Creepster",
       "userid":20165690,
-      "Color":[
+      "TagIdentification":" [ COOL ] ",
+      "ColorTable":[
          0,
          255,
          255
       ],
-      "TagIdentification":"CO",
       "TagText":"Shaun"
    },
    {
       "Font":"Creepster",
       "userid":1417460726,
-      "Color":[
+      "TagIdentification":" [ COOL ] ",
+      "ColorTable":[
          0,
          255,
          255
       ],
-      "TagIdentification":"CO",
       "TagText":"Fergo"
    },
    {
       "Font":"Creepster",
       "userid":1006384408,
-      "Color":[
+      "TagIdentification":" [ COOL ] ",
+      "ColorTable":[
          0,
          255,
          255
       ],
-      "TagIdentification":"CO",
       "TagText":"Updtd"
    },
    {
       "Font":"Creepster",
-      "userid":24158945,
-      "Color":[
+      "userid":2259859528,
+      "TagIdentification":" [ COOL ] ",
+      "ColorTable":[
          0,
          255,
          255
       ],
-      "TagIdentification":"CO",
+      "TagText":"Kenji"
+   },
+   {
+      "Font":"Creepster",
+      "userid":24158945,
+      "TagIdentification":" [ COOL ] ",
+      "ColorTable":[
+         0,
+         255,
+         255
+      ],
       "TagText":"Scottish"
    },
    {
       "Font":"Creepster",
       "userid":94428247,
-      "Color":[
+      "TagIdentification":" [ COOL ] ",
+      "ColorTable":[
          0,
          255,
          255
       ],
-      "TagIdentification":"CO",
       "TagText":"Charlotte"
    },
    {
       "Font":"Fredoka",
       "userid":1937111826,
-      "Color":"Red",
-      "TagIdentification":"RE",
+      "TagIdentification":" [ RETARD ] ",
+      "ColorTable":[
+         255,
+         0,
+         0
+      ],
       "TagText":"SlayesXl"
    },
    {
       "Font":"Fredoka",
       "userid":221789271,
-      "Color":"Red",
-      "TagIdentification":"RE",
+      "TagIdentification":" [ RETARD ] ",
+      "ColorTable":[
+         255,
+         0,
+         0
+      ],
       "TagText":"Townley"
    },
    {
       "Font":"Fredoka",
       "userid":1578148497,
-      "Color":"Red",
-      "TagIdentification":"RE",
+      "TagIdentification":" [ RETARD ] ",
+      "ColorTable":[
+         255,
+         0,
+         0
+      ],
       "TagText":"Townley ( SHARED )"
+   },
+   {
+      "Font":"Fredoka",
+      "userid":348975210,
+      "TagIdentification":" [ RETARD ] ",
+      "ColorTable":[
+         255,
+         0,
+         0
+      ],
+      "TagText":"Hellboy"
+   },
+   {
+      "Font":"Fredoka",
+      "userid":695827286,
+      "TagIdentification":" [ RETARD ] ",
+      "ColorTable":[
+         255,
+         0,
+         0
+      ],
+      "TagText":"Liv"
+   },
+   {
+      "Font":"Fredoka",
+      "userid":436567161,
+      "TagIdentification":" [ RETARD ] ",
+      "ColorTable":[
+         255,
+         0,
+         0
+      ],
+      "TagText":"Vehel"
+   },
+   {
+      "Font":"Fredoka",
+      "userid":2032938950,
+      "TagIdentification":" [ RETARD ] ",
+      "ColorTable":[
+         255,
+         0,
+         0
+      ],
+      "TagText":"BURNTLETTER"
    },
    {
       "Font":"Bangers",
       "userid":355053032,
-      "Color":"RedPink",
-      "TagIdentification":"Custom/FANBOY",
+      "TagIdentification":" [ FANBOY ] ",
+      "ColorTable":[
+         95,
+         12,
+         247
+      ],
       "TagText":"gohnmane"
    },
    {
       "Font":"Classic",
       "userid":1506101592,
-      "Color":"RedPink",
-      "TagIdentification":"SMP",
+      "TagIdentification":" [ SIMP ] ",
+      "ColorTable":[
+         255,
+         0,
+         119
+      ],
       "TagText":"c6pl"
    },
    {
       "Font":"Classic",
+      "userid":56465077,
+      "TagIdentification":" [ SIMP ] ",
+      "ColorTable":[
+         255,
+         0,
+         119
+      ],
+      "TagText":"Finster"
+   },
+   {
+      "Font":"Classic",
+      "userid":1506101592,
+      "TagIdentification":" [ DOGGY ]",
+      "ColorTable":[
+         255,
+         0,
+         119
+      ],
+      "TagText":"LorIIzx"
+   },
+   {
+      "Font":"Classic",
+      "userid":1240822610,
+      "TagIdentification":" [ SIMP ] ",
+      "ColorTable":[
+         255,
+         0,
+         119
+      ],
+      "TagText":"Dxrske"
+   },
+   {
+      "Font":"Classic",
       "userid":75838249,
-      "Color":"RedPink",
-      "TagIdentification":"SMP",
+      "TagIdentification":" [ SIMP ] ",
+      "ColorTable":[
+         255,
+         0,
+         119
+      ],
       "TagText":"JJ"
    },
    {
       "Font":"Classic",
+      "userid":1253699666,
+      "TagIdentification":" [ SIMP ] ",
+      "ColorTable":[
+         255,
+         0,
+         119
+      ],
+      "TagText":"Rabbit"
+   },
+   {
+      "Font":"Classic",
       "userid":1112962991,
-      "Color":"RedPink",
-      "TagIdentification":"SMP",
+      "TagIdentification":" [ SIMP ] ",
+      "ColorTable":[
+         255,
+         0,
+         119
+      ],
       "TagText":"Anti"
    },
    {
       "Font":"Classic",
       "userid":1685050142,
-      "Color":"RedPink",
-      "TagIdentification":"SMP",
+      "TagIdentification":" [ SIMP ] ",
+      "ColorTable":[
+         255,
+         0,
+         119
+      ],
       "TagText":"0shmucks"
    },
    {
       "Font":"Classic",
       "userid":156756616,
-      "Color":"RedPink",
-      "TagIdentification":"Custom/TRYHARD WOMEN",
+      "TagIdentification":" [ TRYHARD WOMEN ] ",
+      "ColorTable":[
+         255,
+         0,
+         119
+      ],
       "TagText":"Alizuuh"
    },
    {
       "Font":"Classic",
       "userid":1260243991,
-      "Color":"RedPink",
-      "TagIdentification":"Custom/TRYHARD WOMEN",
+      "TagIdentification":" [ TRYHARD WOMEN ] ",
+      "ColorTable":[
+         255,
+         0,
+         119
+      ],
       "TagText":"gl8c"
    },
    {
       "Font":"Classic",
       "userid":41032892,
-      "Color":"RedPink",
-      "TagIdentification":"Custom/TRYHARD WOMEN",
+      "TagIdentification":" [ TRYHARD WOMEN ] ",
+      "ColorTable":[
+         255,
+         0,
+         119
+      ],
       "TagText":"Kitncy"
    },
    {
       "Font":"Classic",
       "userid":114239440,
-      "Color":"RedPink",
-      "TagIdentification":"Custom/TRYHARD WOMEN",
+      "TagIdentification":" [ TRYHARD WOMEN ] ",
+      "ColorTable":[
+         255,
+         0,
+         119
+      ],
       "TagText":"Kuiasii"
    },
    {
       "Font":"Classic",
       "userid":177990830,
-      "Color":"RedPink",
-      "TagIdentification":"Custom/AIMLOCKER TRYHARD",
+      "TagIdentification":" [ AIMLOCKER TRYHARD ] ",
+      "ColorTable":[
+         255,
+         0,
+         119
+      ],
       "TagText":"Zisukie"
    },
    {
       "Font":"Creepster",
       "userid":1186330995,
-      "Color":"Yellow",
-      "TagIdentification":"Custom/BOX HEAD",
+      "TagIdentification":" [ BOX HEAD ] ",
+      "ColorTable":[
+         255,
+         255,
+         0
+      ],
       "TagText":"Rheapher"
    },
    {
       "Font":"Creepster",
       "userid":1977510288,
-      "Color":"Yellow",
-      "TagIdentification":"Custom/TOP 1 AFRICAN",
+      "TagIdentification":" [ TOP 1 AFRICAN ] ",
+      "ColorTable":[
+         255,
+         255,
+         0
+      ],
       "TagText":"Berhan"
    },
    {
       "Font":"Creepster",
       "userid":111994154,
-      "Color":"Yellow",
-      "TagIdentification":"Custom/SEX OFFENDER",
+      "TagIdentification":" [ SEX OFFENDER ] ",
+      "ColorTable":[
+         255,
+         255,
+         0
+      ],
       "TagText":"Cherry"
    },
    {
       "Font":"Creepster",
       "userid":147387693,
-      "Color":"Green",
-      "TagIdentification":"TR",
+      "TagIdentification":" [ TRYHARD ] ",
+      "ColorTable":[
+         0,
+         255,
+         0
+      ],
       "TagText":"Demasaii"
    },
    {
       "Font":"Creepster",
+      "userid":1327906710,
+      "TagIdentification":" [ TRYHARD ] ",
+      "ColorTable":[
+         0,
+         255,
+         0
+      ],
+      "TagText":"90BPM"
+   },
+   {
+      "Font":"Creepster",
       "userid":932528290,
-      "Color":"Green",
-      "TagIdentification":"TR",
+      "TagIdentification":" [ TRYHARD ] ",
+      "ColorTable":[
+         0,
+         255,
+         0
+      ],
       "TagText":"Do8m"
    },
    {
       "Font":"Creepster",
+      "userid":92700958,
+      "TagIdentification":" [ TRYHARD ] ",
+      "ColorTable":[
+         0,
+         255,
+         0
+      ],
+      "TagText":"Roro"
+   },
+   {
+      "Font":"Creepster",
+      "userid":1385822787,
+      "TagIdentification":" [ TRYHARD ] ",
+      "ColorTable":[
+         0,
+         255,
+         0
+      ],
+      "TagText":"V9UT"
+   },
+   {
+      "Font":"Creepster",
+      "userid":1385822787,
+      "TagIdentification":" [ TRYHARD ] ",
+      "ColorTable":[
+         0,
+         255,
+         0
+      ],
+      "TagText":"V9UT"
+   },
+   {
+      "Font":"Creepster",
+      "userid":156878502,
+      "TagIdentification":" [ TRYHARD ] ",
+      "ColorTable":[
+         0,
+         255,
+         0
+      ],
+      "TagText":"F6oor"
+   },
+   {
+      "Font":"Creepster",
+      "userid":1576275919,
+      "TagIdentification":" [ TRYHARD ] ",
+      "ColorTable":[
+         0,
+         255,
+         0
+      ],
+      "TagText":"The_L7"
+   },
+   {
+      "Font":"Creepster",
+      "userid":78619360,
+      "TagIdentification":" [ TRYHARD ] ",
+      "ColorTable":[
+         0,
+         255,
+         0
+      ],
+      "TagText":"CellService"
+   },
+   {
+      "Font":"Creepster",
+      "userid":1255702055,
+      "TagIdentification":" [ TRYHARD ] ",
+      "ColorTable":[
+         0,
+         255,
+         0
+      ],
+      "TagText":"TokyoRoka"
+   },
+   {
+      "Font":"Creepster",
+      "userid":1617409512,
+      "TagIdentification":" [ TRYHARD ] ",
+      "ColorTable":[
+         0,
+         255,
+         0
+      ],
+      "TagText":"Cananqtt"
+   },
+   {
+      "Font":"Creepster",
+      "userid":329574172,
+      "TagIdentification":" [ TRYHARD ] ",
+      "ColorTable":[
+         0,
+         255,
+         0
+      ],
+      "TagText":"Kev"
+   },
+   {
+      "Font":"Creepster",
       "userid":1579693798,
-      "Color":"Green",
-      "TagIdentification":"TR",
+      "TagIdentification":" [ TRYHARD ] ",
+      "ColorTable":[
+         0,
+         255,
+         0
+      ],
       "TagText":"Remi"
    },
    {
       "Font":"Creepster",
       "userid":316508463,
-      "Color":"Green",
-      "TagIdentification":"Custom/TRYHARD 'TOP 1'",
+      "TagIdentification":" [ TRYHARD / 'TOP 1' ] ",
+      "ColorTable":[
+         0,
+         255,
+         0
+      ],
       "TagText":"Josh"
    },
    {
       "Font":"Creepster",
       "userid":971790374,
-      "Color":"Green",
-      "TagIdentification":"Custom/TOP 1 TRIM",
+      "TagIdentification":" [ TOP 1 TRIM ] ",
+      "ColorTable":[
+         0,
+         255,
+         0
+      ],
       "TagText":"Punk"
+   },
+   {
+      "Font":"Creepster",
+      "userid":116974029,
+      "TagIdentification":"[ RETARD ] ",
+      "ColorTable":[
+         255,
+         0,
+         0
+      ],
+      "TagText":"NOCONTEXT"
+   },
+   {
+      "Font":"Creepster",
+      "userid":97483119,
+      "TagIdentification":" [ SIMP ] ",
+      "ColorTable":[
+         255,
+         0,
+         119
+      ],
+      "TagText":"Michskii"
+   },
+   {
+      "Font":"Classic",
+      "userid":147106532,
+      "TagIdentification":" [ WEIRD ] ",
+      "ColorTable":[
+         255,
+         69,
+         156
+      ],
+      "TagText":"Shauns GF"
+   },
+   {
+      "Font":"Classic",
+      "userid":542844700,
+      "TagIdentification":" [ HOMOSEXUAL GUY ] ",
+      "ColorTable":[
+         255,
+         255,
+         0
+      ],
+      "TagText":"Yuhxra"
+   },
+   {
+      "Font":"Classic",
+      "userid":542844700,
+      "TagIdentification":" [ TRYHARD ] ",
+      "ColorTable":[
+         0,
+         255,
+         0
+      ],
+      "TagText":"zatnz"
+   },
+   {
+      "Font":"Classic",
+      "userid":969290732,
+      "TagIdentification":" [ TRYHARD ] ",
+      "ColorTable":[
+         0,
+         255,
+         0
+      ],
+      "TagText":"Lewis"
+   },
+   {
+      "Font":"Classic",
+      "userid":1737676452,
+      "TagIdentification":" [ TRYHARD ] ",
+      "ColorTable":[
+         0,
+         255,
+         0
+      ],
+      "TagText":"FanOfNishioka"
+   },
+   {
+      "Font":"Classic",
+      "userid":1561373499,
+      "TagIdentification":" [ TRYHARD ] ",
+      "ColorTable":[
+         0,
+         255,
+         0
+      ],
+      "TagText":"YouMightBeASped"
+   },
+   {
+      "Font":"Classic",
+      "userid":1574565539,
+      "TagIdentification":" [ TRYHARD ] ",
+      "ColorTable":[
+         0,
+         255,
+         0
+      ],
+      "TagText":"gziria"
+   },
+   {
+      "Font":"Classic",
+      "userid":1574565539,
+      "TagIdentification":" [ EXPLOITER ] ",
+      "ColorTable":[
+         0,
+         0,
+         255
+      ],
+      "TagText":"NoDripJosh"
+   },
+   {
+      "Font":"Classic",
+      "userid":2039870027,
+      "TagIdentification":" [ TROLL ] ",
+      "ColorTable":[
+         255, 
+         162, 
+         13
+      ],
+      "TagText":"Mittalt"
+   },
+   {
+      "Font":"Classic",
+      "userid":351456158,
+      "TagIdentification":" [ TROLL ] ",
+      "ColorTable":[
+         255, 
+         162, 
+         13
+      ],
+      "TagText":"Fraizer"
+   },
+   {
+      "Font":"Classic",
+      "userid":1905951531,
+      "TagIdentification":" [ TROLL ] ",
+      "ColorTable":[
+         255, 
+         162, 
+         13
+      ],
+      "TagText":"Shonton"
    }
 ]
